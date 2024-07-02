13ed1c6312446e11f4bc3fc73be1d2b9e4a3b024
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-07-02 19:09:54 -0300

    refactoring interests component

--- src/app/components/Interests/Interests.jsx
+++ src/app/components/Interests/Interests.jsx
@@ -1,3 +1,17 @@
-export default function () {
-  return <section></section>;
+import style from "./interests.module.css";
+
+export default function Interests() {
+  return (
+    <section className={style.interestsContainer}>
+      <h2>Mis intereses</h2>
+      <p>Fuera de la programacion tengo una atraccion por estas actividades:</p>
+      <ul className={style.list}>
+        <li>Musica (Escuchar, cantar, componer)</li>
+        <li>Pasear y disfrutar el tiempo con amigos</li>
+        <li>Poesia y Lectura</li>
+        <li>Meditacion</li>
+        <li>Organizar mi vida</li>
+      </ul>
+    </section>
+  );
 }
