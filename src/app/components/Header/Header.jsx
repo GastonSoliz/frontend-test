1ab8050623875d2904a2537a5bd1ff5469699cbf
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-07-02 19:07:26 -0300

    refactoring header component

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -1,3 +1,19 @@
+import Image from "next/image";
+import style from "./header.module.css";
+
 export default function Header() {
-  return <header></header>;
+  return (
+    <header className={style.headerContainer}>
+      <div className={style.image}>
+        <Image
+          src="/Profile-Transparent.png"
+          alt="Gaston Soliz"
+          width={160}
+          height={200}
+          priority
+        />
+      </div>
+      <h1>Gaston Soliz</h1>
+    </header>
+  );
 }
